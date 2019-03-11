d89c49ca8cd47e395fbea5403227f01a7e409d98
Michał Prusak <meehoweq@gmail.com>
2019-03-11 03:45:30 +0100

    Nowy moduł do obsługi forum.

---
+++ Discord/Modules/CommunityModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Utilities;
+using Microsoft.Extensions.Configuration;
+
+namespace Discord.Modules
+{
+    [Name("Invision Community")]
+    // ReSharper disable once UnusedMember.Global
+    public class CommunityModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly CommandService _service;
+        private readonly IConfigurationRoot _config;
+
+        public CommunityModule(CommandService service, IConfigurationRoot config)
+        {
+            _service = service;
+            _config = config;
+        }
+
+        [Command("findmember")]
+        [Summary("Znajduje użytkownika forum po jego ID i wyświetla detale jego konta.")]
+        [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        // ReSharper disable once UnusedMember.Global
+        public async Task PruneAsync(int memberId)
+        {
+            await ReplyAsync(memberId.ToString());
+        }
+    }
+}
