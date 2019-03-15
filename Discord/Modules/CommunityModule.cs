2f2bda8fc8adfad802caaeeaf751c849ad68a8f4
Michał Prusak <meehoweq@gmail.com>
2019-03-15 01:21:37 +0100

    Społeczność - ogłoszenia.

--- Discord/Modules/CommunityModule.cs
+++ Discord/Modules/CommunityModule.cs
@@ -1,39 +1,62 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.Utilities;
 using Microsoft.Extensions.Configuration;
 
 namespace Discord.Modules
 {
-    [Name("Invision Community")]
+    [Name("Społeczność")]
     // ReSharper disable once UnusedMember.Global
     public class CommunityModule : ModuleBase<SocketCommandContext>
     {
-        private readonly CommandService _service;
         private readonly IConfigurationRoot _config;
 
-        public CommunityModule(CommandService service, IConfigurationRoot config)
+        public CommunityModule(IConfigurationRoot config)
         {
-            _service = service;
             _config = config;
         }
 
-        [Command("findmember")]
-        [Summary("Znajduje użytkownika forum po jego ID i wyświetla detale jego konta.")]
+        [Command("announce")]
+        [Summary("Tworzy nowe ogłoszenie na Discordzie.")]
         [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.Administrator)]
         // ReSharper disable once UnusedMember.Global
-        public async Task FindMemberAsync(int id)
+        public async Task AnnounceAsync([Remainder] string text)
         {
-            await ReplyAsync(id.ToString());
-        }
-        
-        [Command("findmember")]
-        [Summary("Znajduje użytkownika forum po jego nazwie i wyświetla detale jego konta.")]
-        [RequireContext(ContextType.Guild)]
-        // ReSharper disable once UnusedMember.Global
-        public async Task FindMemberAsync(string name)
-        {
-            await ReplyAsync(name);
+            if (_config["channels:announcements"] == null)
+            {
+                await ReplyAsync("", false, Embeds.Error("Nie uzupełniono ID kanału ogłoszeń " +
+                                                         "w pliku konfiguracyjnym. Nie mogę wysłać ogłoszenia."));
+                return;
+            }
+
+            var embed = new EmbedBuilder()
+            {
+                Color = new Color(255, 109, 0),
+                Title = "Ogłoszenie",
+                Description = text
+            }.WithCurrentTimestamp().Build();
+
+            try
+            {
+                var channel = Context.Guild.GetTextChannel(ulong.Parse(_config["channels:announcements"]));
+
+                if (channel == null)
+                {
+                    await ReplyAsync("", false, Embeds.Error("Nie mogę odnaleźć " +
+                                                             "kanału ogłoszeń. Sprawdź, czy taki kanał istnieje."));
+                    return;
+                }
+                
+                await channel.SendMessageAsync("", false, embed);
+            } 
+            catch (Exception) 
+            {
+                await ReplyAsync("", false, Embeds.Error("Nie mogę odnaleźć kanału " +
+                                                         "ogłoszeń. Sprawdź poprawność pliku konfiguracyjnego."));
+            }
+            
         }
     }
 }
